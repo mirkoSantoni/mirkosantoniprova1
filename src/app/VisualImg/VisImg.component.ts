ead6d98cf2d4e1d46593bbd0cca8a262e48c6e53
mirkoSantoni <mirko.santoni@marconirovereto.it>
2021-02-18 15:46:48 +0100

    1 file modified

--- src/app/VisualImg/VisImg.component.ts
+++ src/app/VisualImg/VisImg.component.ts
@@ -22,15 +22,15 @@ export class VisImgComponent {
 
   btnCliccato() {
     if (this.scelta) {
-      this.nomeSocieta = "Audi";
-      this.articolo = "dell'";
-      this.percorso =
-        "https://images.everyeye.it/img-notizie/ecco-audi-rs-e-tron-gt-bolide-elettrico-mostra-angolazione-v4-479147-1280x720.jpg";
-    } else {
-      this.nomeSocieta = "Ferrari";
+      this.nomeSocieta = "Lamborghini";
       this.articolo = "della ";
       this.percorso =
-        "https://media.magazine.ferrari.com/images/2019/03/25/112037181-ade321bc-2a1b-4a66-952f-d845c44073e8.jpg";
+        "https://github.com/mirkoSantoni/image4BI/blob/main/lamborghini.jpg?raw=true";
+    } else {
+      this.nomeSocieta = "Maserati";
+      this.articolo = "della ";
+      this.percorso =
+        "https://github.com/mirkoSantoni/image4BI/blob/main/maserati.jpg?raw=true";
     }
     this.scelta = !this.scelta;
     this.numero++;
